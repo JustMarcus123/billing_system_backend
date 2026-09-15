5d5a250e6026c61b25fed9b26b902f62fa8ac970
JustMarcus123 <141229878+JustMarcus123@users.noreply.github.com>
2026-09-16 03:29:40 +0530

    health check

---
+++ src/main/java/com/billlingSystem/BillingSystem/Auth/AuthEntity.java
@@ -0,0 +1,15 @@
+package com.billlingSystem.BillingSystem.Auth;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+@Table(name = "")
+public class AuthEntity {
+
+}
